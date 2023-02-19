c68c1ae3276de914af62ca2ed4e0c8875dbf866e
judidoko <judidoko@gmail.com>
2023-02-19 09:21:20 +0100

    Js challange

--- designerPdfViewer.js
+++ designerPdfViewer.js
@@ -1,4 +1,15 @@
+let Word =[1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7];
 function designerPdfViewer(h, word) {
     // Write your code here
-
+  let tallestLetter = 0;
+  let wordArr = word.split('');
+  
+  for(let i=0; i< h.length; i++) {
+    if (h[i] > tallestLetter) {
+      tallestLetter = h[i];
+    }
+  }
+  return tallestLetter * wordArr.length;
 }
+let result = designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], "zaba");
+console.log(result);
