6b72cd041f9f6016076f3d268fdb6969a05636c6
GeekyHacks <geekyhacks22@gmail.com>
2023-02-19 11:33:51 +0300

    push

--- designerPdfViewer.js
+++ designerPdfViewer.js
@@ -1,15 +1,58 @@
-let Word =[1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7];
+let Word = [
+  1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+  7,
+];
 function designerPdfViewer(h, word) {
-    // Write your code here
-  let tallestLetter = 0;
-  let wordArr = word.split('');
+  let wordHights = [
+    1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+    5, 7,
+  ];
+
+  let AlphbitArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
   
-  for(let i=0; i< h.length; i++) {
+  let tallLitWord = 0;
+  let wordArr = word.split("");
+
+  for (let i = 0; i < h.length; i++) {
     if (h[i] > tallestLetter) {
       tallestLetter = h[i];
     }
   }
-  return tallestLetter * wordArr.length;
+  return tallestLetter * wordHights.length;
 }
-let result = designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], "zaba");
+let result = designerPdfViewer(
+  [
+    1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+    5, 7,
+  ],
+  "zaba"
+);
 console.log(result);
